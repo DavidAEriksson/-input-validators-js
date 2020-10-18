9fe74d216b37c1ba71a2d188ef8b7d8f077f9cc9
David Eriksson <daer3303@student.umu.se>
2020-10-18 19:01:14 +0200

    Revert to older version.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # input-validators-js
 
 A simple npm-package for validating user input like `email`, `password`-strength, `date`-format and so on.
@@ -92,3 +93,47 @@ validate.validateTypeNumber(number);
 ```
 
 This will return `true` if the object passed is a number and `false` if not.
+=======
+module.exports.validateEmail = (email) => {
+	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+module.exports.validateName = (name) => {
+	return /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/.test(
+		name
+	);
+};
+
+module.exports.validatePasswordStrength = (password) => {
+	var strongRegex = new RegExp(
+		"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+	).test(password);
+	var mediumRegex = new RegExp(
+		"^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
+	).test(password);
+
+	if (strongRegex) {
+		return { passwordAccepptable: strongRegex, passwordStrength: "strong" };
+	} else if (mediumRegex) {
+		return { passwordAccepptable: mediumRegex, passwordStrength: "medium" };
+	} else return { passwordAccepptable: false, passwordStrength: "weak" };
+};
+
+module.exports.validateDateFormatDayMonthYear = (date) => {
+	return /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
+		date
+	);
+};
+
+module.exports.validateDateFormatYearMonthDay = (date, format) => {
+	if (format === 0) {
+		return /^\d{4}[\/.]\d{1,2}[\/.]\d{1,2}$/.test(date);
+	} else if (format === 1) {
+		return /^\d{4}\-\d{1,2}\-\d{1,2}$/.test(date);
+	} else if (format === 2) {
+		return /(\d{4})-(\d{2})-(\d{2})/.test(date);
+	} else {
+		return console.error(`Wrong parameter: ${format}. Expected values 0, 1, 2`);
+	}
+};
+>>>>>>> parent of 0049f12... Added validation for string type and number type
