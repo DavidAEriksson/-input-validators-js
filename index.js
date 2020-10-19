e35f3b218d6c7b033a538babf4350431645f806d
David Eriksson <david_ux@outlook.com>
2020-10-19 10:58:54 +0200

    Fixed typo in password return

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 module.exports.validateEmail = (email) => {
-	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
 
 module.exports.validateName = (name) => {
-	return /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/.test(
-		name
-	);
+  return /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/.test(
+    name
+  );
 };
 
 module.exports.validatePasswordStrength = (password) => {
-	var strongRegex = new RegExp(
-		"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
-	).test(password);
-	var mediumRegex = new RegExp(
-		"^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
-	).test(password);
+  var strongRegex = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  ).test(password);
+  var mediumRegex = new RegExp(
+    "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
+  ).test(password);
 
-	if (strongRegex) {
-		return { passwordAccepptable: strongRegex, passwordStrength: "strong" };
-	} else if (mediumRegex) {
-		return { passwordAccepptable: mediumRegex, passwordStrength: "medium" };
-	} else return { passwordAccepptable: false, passwordStrength: "weak" };
+  if (strongRegex) {
+    return { passwordAcceptable: strongRegex, passwordStrength: "strong" };
+  } else if (mediumRegex) {
+    return { passwordAcceptable: mediumRegex, passwordStrength: "medium" };
+  } else return { passwordAcceptable: false, passwordStrength: "weak" };
 };
 
 module.exports.validateDateFormatDayMonthYear = (date) => {
-	return /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
-		date
-	);
+  return /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
+    date
+  );
 };
 
 module.exports.validateDateFormatYearMonthDay = (date, format) => {
-	if (format === 0) {
-		return /^\d{4}[\/.]\d{1,2}[\/.]\d{1,2}$/.test(date);
-	} else if (format === 1) {
-		return /^\d{4}\-\d{1,2}\-\d{1,2}$/.test(date);
-	} else if (format === 2) {
-		return /(\d{4})-(\d{2})-(\d{2})/.test(date);
-	} else {
-		return console.error(`Wrong parameter: ${format}. Expected values 0, 1, 2`);
-	}
+  if (format === 0) {
+    return /^\d{4}[\/.]\d{1,2}[\/.]\d{1,2}$/.test(date);
+  } else if (format === 1) {
+    return /^\d{4}\-\d{1,2}\-\d{1,2}$/.test(date);
+  } else if (format === 2) {
+    return /(\d{4})-(\d{2})-(\d{2})/.test(date);
+  } else {
+    return console.error(`Wrong parameter: ${format}. Expected values 0, 1, 2`);
+  }
 };
 
 module.exports.validateTypeString = (str) => {
-	return typeof str === "string";
+  return typeof str === "string";
 };
 
 module.exports.validateTypeNumber = (num) => {
-	return typeof num == "number";
+  return typeof num == "number";
 };
